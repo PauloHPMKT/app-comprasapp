c458ebc592efa12c65d656816341b85673e5e92b
Paulo Sergio <pauloserg861@gmail.com>
2025-10-07 09:04:58 -0300

    refactor: modify template to change logo and apply load skelleton

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -4,25 +4,33 @@ import { Icon } from '@iconify/vue';
 import { useRouter } from 'vue-router';
 import Content from './Content.vue';
 import ProfileCard from '../components/ProfileCard.vue';
-import horizontalLogo from '../assets/img/Logo-horizontal.png'
+import horizontalLogo from '../assets/img/comprasapp-horizontal-logo.png'
 import { useAuthStore } from '../store/auth';
+import { useLoadingStore } from '../store/loading';
 
 const authStore = useAuthStore();
+const loadingStore = useLoadingStore();
 const router = useRouter();
 
 const userProfileCard = ref<typeof ProfileCard | null>(null);
 
+const isLoading = computed(() => loadingStore.isLoading);
+
+const isLoggedIn = computed(() => {
+  return authStore.isAuthenticated;
+});
+
 const currentUser = computed(() => {
-  return authStore.$state.currentUser;
+  return authStore.$state?.currentUser;
 });
 
 const firstNameUpperLatter = computed(() => {
-  return currentUser.value.name.charAt(0).toUpperCase();
-})
+  return currentUser.value?.name?.charAt(0)?.toUpperCase() ?? '';
+});
 
 const username = computed(() => {
-  return currentUser.value.name;
-})
+  return currentUser.value?.name ?? '';
+});
 
 function toCategories() {
   localStorage.removeItem('purchase-list-title');
@@ -35,25 +43,42 @@ function toggleProfileCard() {
 </script>
 
 <template>
-  <div class="min-h-[100dvh] flex flex-col">
+  <!-- Esse header pode virar um componente -->
+  <header class="flex justify-between items-center font-bold sm:px-32 py-3 px-3 border-b border-gray-100 bg-white">
+    <div>
+      <div
+        v-if="isLoading"
+        class="w-44 h-14 rounded-lg animate-pulse flex items-center justify-center" style="animation-duration: 1.5s;">
+        <div class="w-full h-full bg-gray-200 rounded-lg"></div>
+      </div>
+      <div v-else class="bg-white p-2 w-36 sm:w-44">
+        <img :src="horizontalLogo" alt="Logo Comprasapp horizonta" class="w-full" />
+      </div>
+    </div>
+    <div
+      v-if="isLoggedIn"
+      class="flex items-center justify-center gap-4"
+    >
+      <div class="flex items-center gap-2 cursor-pointer relative" @click="toggleProfileCard">
+        <span
+          v-if="isLoggedIn"
+          class="bg-gray-700 rounded-full w-12 h-12 flex items-center justify-center text-white cursor-pointer">
+          {{ firstNameUpperLatter }}
+        </span>
+        <strong class="hidden sm:block">{{ username }}</strong>
+        <ProfileCard ref="userProfileCard" />
+      </div>
+    </div>
+  </header>
+
+  <div class="min-h-[90dvh] flex flex-col">
     <div class="w-full max-w-[1280px] mx-auto flex-1 flex flex-col">
-      <div class="mx-6 flex-1 flex flex-col">
-        <header class="flex items-center justify-between py-4">
-          <div class="bg-white p-2 rounded-md shadow-md/5 w-36 sm:w-44">
-            <img :src="horizontalLogo" alt="Logo Comprasapp horizonta" class="w-full" />
-          </div>
-          <div class="flex items-center justify-center gap-4">
-            <div class="flex items-center gap-2 cursor-pointer relative" @click="toggleProfileCard">
-              <span class="bg-gray-700 rounded-full w-12 h-12 flex items-center justify-center text-white cursor-pointer">
-                {{ firstNameUpperLatter }}
-              </span>
-              <strong class="hidden sm:block">{{ username }}</strong>
-              <ProfileCard ref="userProfileCard" />
-            </div>
-          </div>
-        </header>
-        <div class="flex shadow-sm/5 bg-white border border-gray-100 rounded-lg flex-1 pr-4 overflow-hidden">
-          <div class="w-60 border-r-2 border-gray-100 bg-gray-50 pr-4 hidden sm:block">
+      <div class="sm:mx-6 mx-0 flex-1 flex flex-col">
+        <div class="flex shadow-sm/5 bg-white flex-1 pr-4 overflow-hidden">
+          <div
+            v-if="isLoggedIn"
+            class="w-60 border-r-2 border-gray-100 bg-gray-50 pr-4 hidden sm:block"
+          >
             <ul class="flex flex-col items-start justify-start p-4">
               <li class="flex items-center justify-between mb-4">
                 <span class="text-gray-500">Minhas Listas</span>
